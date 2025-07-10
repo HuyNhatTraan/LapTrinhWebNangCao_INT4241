de574c47400d330716ced6accab6ed6a1d67d9c6
HuyNhatTran <Huy11042k4@gmail.com>
2025-07-10 21:54:06 +0700

    refactor: update navigation links and add new page templates

---
+++ frontend/pages/support/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/LapTrinhWebNangCao_INT4241/frontend/styles/output.css">
+    <script src="/LapTrinhWebNangCao_INT4241/frontend/scripts/index.js"></script>
+    <title>HT Tech Official Store</title>
+</head>
+<body>
+    <div class="" id="nav"></div>
+    <div class="" id="footer"></div>
+</body>
+</html>
