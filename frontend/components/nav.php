bd69c94d84c10afe49312f2fd1c13e9b8bc6d9c5
HuyNhatTran <Huy11042k4@gmail.com>
2025-07-25 11:18:15 +0700

    refactor: improve navigation structure and styling for better user experience

--- frontend/components/nav.php
+++ frontend/components/nav.php
@@ -1,57 +1,67 @@
 <?php session_start(); ?>
-<nav class="sticky top-0 z-35">
-    <div class="flex h-14 items-center bg-white justify-between shadow-md">
-        <div class="flex gap-4 ml-10 xl:ml-55 lg:ml-30 md:ml-10 sm:ml-10 md:gap-4 !lg:gap-6 items-center">
-            <div class="flex items-center gap-4">
-                <div class="flex md:hidden">
-                    <svg class="w-10 h-10" fill="#000000" viewBox="0 0 32 32" enable-background="new 0 0 32 32" id="Glyph" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M26,16c0,1.104-0.896,2-2,2H8c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C25.104,14,26,14.896,26,16z" id="XMLID_314_"></path><path d="M26,8c0,1.104-0.896,2-2,2H8c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C25.104,6,26,6.896,26,8z" id="XMLID_315_"></path><path d="M26,24c0,1.104-0.896,2-2,2H8c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C25.104,22,26,22.896,26,24z" id="XMLID_316_"></path></g></svg>
-                </div>
-                <a href="/LapTrinhWebNangCao_INT4241/frontend/">
-                    <img class="w-8 h-8" src="/LapTrinhWebNangCao_INT4241/frontend/icon.png" alt="">
-                </a>
-            </div>         
-            <div class="hidden font-sans gap-4 md:flex md:gap-4 lg:gap-6 2xl:gap-10">
-                <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/store">
-                    <span class="cursor-pointer text-lg 2xl:text-sm xl:text-sm lg:text-sm md:text-[12px] hover:text-[#3c81c6] duration-150">
-                        Cửa hàng</span>                      
-                </a>
-                <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/mobile"><span
-                        class="cursor-pointer text-lg 2xl:text-sm 2xl:font-normal xl:text-sm lg:text-sm md:text-[12px] hover:text-[#3c81c6] duration-150">
-                        Di động</span>
-                </a>
-                <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/smart-home"><span
-                        class="cursor-pointer text-lg 2xl:text-sm 2xl:font-normal xl:text-sm lg:text-sm md:text-[12px] hover:text-[#3c81c6] duration-150">
-                        Nhà thông minh</span>
-                </a>                         
-            </div>  
-        </div>
-        <div class="flex mr-10 xl:mr-55 gap-10 lg:gap-6 lg:mr-30 md:mr-10 sm:mr-10 md:gap-10 items-center">
-            <div class="hidden gap-2 md:flex md:gap-4 lg:gap-6 2xl:gap-10">
-                <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/discover"><span
-                    class="cursor-pointer text-lg 2xl:text-sm 2xl:font-normal xl:text-sm lg:text-sm md:text-[12px] hover:text-[#3c81c6] duration-150">
-                    Khám phá</span></a>
-                <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/support"><span
-                    class="cursor-pointer text-lg 2xl:text-sm 2xl:font-normal xl:text-sm lg:text-sm md:text-[12px] hover:text-[#3c81c6] duration-150">
-                    Hỗ trợ</span></a>
-            </div>
-            <div class="flex gap-4 md:gap-4 lg:gap-6 2xl:gap-10">
-                <a href="">
-                <svg class="w-6 h-6 hover:stroke-[#3c81c6] duration-150" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
-            </a>
-            <a href="">
-                <svg class="w-6 h-6 hover:stroke-[#3c81c6] duration-150" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
-            </a>
-
-            <?php if (isset($_SESSION['user'])): ?>
-                <div class="flex gap-4">
+<nav class="bg-white border-gray-200 top-0 z-50 sticky">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/LapTrinhWebNangCao_INT4241/frontend/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img class="w-8 h-8" src="/LapTrinhWebNangCao_INT4241/frontend/icon.png" alt="HT Tech Logo">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">HT Tech</span>
+        </a>
+        <button data-collapse-toggle="navbar-default" type="button"
+            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul
+                class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:border-gray-700">
+                <li>
+                    <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/store/"
+                        class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-black md:dark:text-blue-500"
+                        aria-current="page">Cửa hàng</a>
+                </li>
+                <li>
+                    <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/mobile/"
+                        class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Di
+                        động</a>
+                </li>
+                <li>
+                    <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/smart-home/"
+                        class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Nhà
+                        thông minh</a>
+                </li>
+                <li>
+                    <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/discover/"
+                        class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Khám
+                        Phá</a>
+                </li>
+                <li>
+                    <a href="/LapTrinhWebNangCao_INT4241/frontend/pages/support/"
+                        class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Hỗ
+                        trợ</a>
+                </li>
+                <li>
+                    <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                    <svg class="w-6 h-6 hover:stroke-[#3c81c6] duration-150" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <svg class="w-6 h-6 hover:stroke-[#3c81c6] duration-150" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <?php if (isset($_SESSION['user'])): ?>                   
                     <a href="/LapTrinhWebNangCao_INT4241/frontend/services/account.php">
                         <svg class="w-6 h-6 hover:stroke-[#3c81c6] duration-150" xmlns="http://www.w3.org/2000/svg"
                             fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -59,19 +69,18 @@
                                 d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                         </svg>
                     </a>
-                </div>
-
-            <?php else: ?>
-                <a href="/LapTrinhWebNangCao_INT4241/frontend/services/login.php">
-                    <svg class="w-6 h-6 hover:stroke-[#3c81c6] duration-150" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                <?php else: ?>
+                    <a href="/LapTrinhWebNangCao_INT4241/frontend/services/login.php">
+                        <svg class="w-6 h-6 hover:stroke-[#3c81c6] duration-150" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                         </svg>
                     </a>
-            <?php endif; ?>
-            </div>
-            
+                <?php endif; ?>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
+<script defer src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
