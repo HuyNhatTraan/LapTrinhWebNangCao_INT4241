a4ceaba5186540e57a85082a667c6dcc61922f2c
HuyNhatTran <Huy11042k4@gmail.com>
2025-07-25 20:49:06 +0700

    refactor: update file paths for styles and icons to relative paths in discover, smart-home, and support pages

--- app/views/pages/discover/index.php
+++ app/views/pages/discover/index.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/LapTrinhWebNangCao_INT4241/frontend/styles/output.css">
-    <script src="/LapTrinhWebNangCao_INT4241/frontend/scripts/index.js"></script>
-    <link rel="icon" href="/LapTrinhWebNangCao_INT4241/frontend/icon.png" type="image/png"/>
+    <link rel="stylesheet" href="styles/output.css">
+    <link rel="icon" href="icon.png" type="image/png"/>
     <title>HT Tech Official Store</title>
 </head>
 <body>
-    <div class="" id="nav"></div>
-    <div class="" id="footer"></div>
+    <?php require 'views/components/nav.php'; ?>
+
+        
+    <?php require 'views/components/footer.php'; ?>
 </body>
 </html>
--- app/views/pages/smart-home/index.php
+++ app/views/pages/smart-home/index.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/LapTrinhWebNangCao_INT4241/frontend/styles/output.css">
-    <link rel="icon" href="/LapTrinhWebNangCao_INT4241/frontend/icon.png" type="image/png" />
+    <link rel="stylesheet" href="styles/output.css">
+    <link rel="icon" href="icon.png" type="image/png" />
     <title>HT Tech Official Store</title>
 </head>
 
