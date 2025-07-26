13ac1df89da31705f46c081de413fc9d8b090a00
HuyNhatTran <Huy11042k4@gmail.com>
2025-07-26 20:25:38 +0700

    feat: update navigation structure and enhance product page with font preconnects and styles

--- app/views/product/index.php
+++ app/views/product/index.php
@@ -7,6 +7,9 @@
     <link rel="icon" href="icon.png" type="image/png" />
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="styles/output.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <title>Sản phẩm</title>
 </head>
 
@@ -68,7 +71,7 @@
                         </button>
                     </div>
                 </div>
-                <form action="index.php?act=cart-add" method="POST">
+                <form action="" method="POST">
                     <div class="xl:p-15 p-6 flex flex-col">
                         <?php foreach ($products as $product): ?>
                             <input type="hidden" name="MaSP" value="<?= htmlspecialchars($product['MaSP']) ?>">
